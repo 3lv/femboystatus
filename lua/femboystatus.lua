8815f09f5581f67f2ce1e9f198ae928f352a1a16
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-07 13:21:26 +0300

    simplify test2

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -1,14 +1,9 @@
 M = { }
 
-local function Call_Predefined_Function( Function_Name )
-	return require('femboystatus.functions')[Function_Name]()
-end
-
 local function Set_statusline()
 	vim.wo.statusline = [[%{luaeval("require('stlfunctions')['file_name']())]]
 end
 
-M.Function = Call_Predefined_Function()
 M.Set_statusline = Set_statusline()
 
 return M
