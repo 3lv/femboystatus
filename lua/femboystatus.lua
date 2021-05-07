bd72fe1c9ba7279fe2c99bb816aaec9ee77d5b59
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-07 15:02:53 +0300

    differect stl for current and NC windows (test1)

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -1,7 +1,29 @@
 M = { }
 
-local function setup()
-	vim.o.statusline = 
+local fn = vim.fn
+
+-- set's status line for current window and a 
+-- different status line for other windows
+-- (using vim.wo.statusline instead of .o.)
+local function set_statusline()
+
+	nr_of_windows = fn.winnr('$')
+	for winnr in 1, nr_of_windows do -- for each window
+		-- get the id of window i
+		winid = fn.win_getid(i)
+		-- TODO: check for special windows
+		vim.wo[winid]a.statusline = StatusLineNC
+	end
+
+	-- now for current window
+	vim.wo.statusline = StatusLine
+end
+
+
+local function setup( I )
+
+-- Status line used for CURRENT window
+	StatusLine =
 
 [[%{luaeval("require('stlfunctions').File()")}]].. 
 
@@ -12,6 +34,19 @@ local function setup()
 [[%=]]..
 
 [[fem@boy]]
+
+-- Status line used for NOT CURRENT window
+	StatusLineNC = 
+[[%{luaeval("require('stlfunctions').File()")}]].. 
+
+[[%=]]..
+
+
+[[%=]]..
+
+[[<]]
+
+	set_statusline()
 end
 
 M.setup = setup
