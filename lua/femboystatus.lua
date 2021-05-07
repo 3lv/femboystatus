5778ff3f1d402a048d45af8cd83cd50d392e5f87
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-07 16:22:13 +0300

    ipair() lmao

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -57,7 +57,7 @@ local function setup( I )
 
 	vim.api.nvim_command('augroup StatusLine')
 	vim.api.nvim_command('autocmd!')
-	for _,event in ipair(events) do
+	for _,event in ipairs(events) do
 		local command = string.fromat('autocmd %s * lua require("femboystatus").every()', event)
 		vim.api.nvim_command(command)
 	end
