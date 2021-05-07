45a318644ea5d45f3feb4975e910efa19ef53453
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-07 13:27:08 +0300

    should be functional now

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -4,6 +4,6 @@ local function Set_statusline()
 	vim.wo.statusline = [[%{luaeval("require('stlfunctions')['file_name']()")}]]
 end
 
-M.Set_statusline = Set_statusline()
+M.Set_statusline = Set_statusline
 
 return M
