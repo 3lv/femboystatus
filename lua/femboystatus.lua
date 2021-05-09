0e5f38043020125913998fb15ff06123d7cac0dc
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-09 19:57:26 +0300

    debug cor

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -69,7 +69,7 @@ local function set_every_statusline()
 
 	nr_of_windows = fn.winnr('$')
 	current_winid = fn.win_getid()
-	pritn (current_winid)
+	print (current_winid)
 	-- find buffer for current window
 
 	-- Setting statusline for non-current windows
