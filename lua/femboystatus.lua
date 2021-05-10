5405474474cc4651de9ed84e700da1b8c9a90f25
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-10 19:40:54 +0300

    forgot ()

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -27,7 +27,7 @@ local function generate_StatusLine()
 [[%#StatusLineMode#]]..[[%{luaeval("require('stlfunctions').Mode()")}]]..
 [[%=]]..
 
-[[%#Rainbow#]] .. [[%{luaeval("require('stlfunctions').Rainbow")}]]
+[[%#Rainbow#]] .. [[%{luaeval("require('stlfunctions').Rainbow()")}]]
 end
 
 -- Generates a statusline used for current
