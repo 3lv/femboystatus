c5b3fdd2f943b8d23e847c43e7ac4c433b0d4d2c
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-12 12:10:09 +0300

    StatusLine_augroup() typo

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -166,7 +166,7 @@ end
 
 local function setup( I )
 	StatusLine_special_filetype = { NvimTree = 'NvimTree פּ', packer = 'Packer ' }
-	StautsLine_augroup()
+	StatusLine_augroup()
 	vim.api.nvim_command('hi StatusLineFileActive guifg=#b0da6c guibg=NONE gui=bold')
 	vim.api.nvim_command('hi StatusLineFileNoneActive guifg=#b0da6c guibg=NONE')
 
