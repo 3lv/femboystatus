7522592d02a9f5628c457c3976a736b02be07449
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-12 12:11:33 +0300

    moved StatusLine_augroup() as the last function call

--- lua/femboystatus.lua
+++ lua/femboystatus.lua
@@ -166,9 +166,9 @@ end
 
 local function setup( I )
 	StatusLine_special_filetype = { NvimTree = 'NvimTree פּ', packer = 'Packer ' }
-	StatusLine_augroup()
 	vim.api.nvim_command('hi StatusLineFileActive guifg=#b0da6c guibg=NONE gui=bold')
 	vim.api.nvim_command('hi StatusLineFileNoneActive guifg=#b0da6c guibg=NONE')
+	StatusLine_augroup()
 
 end
 
