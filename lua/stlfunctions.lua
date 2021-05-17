545760aa9ea7a7155975a421b7ea576250e3ccb6
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-17 13:13:49 +0300

    correct the colors lmao

--- lua/stlfunctions.lua
+++ lua/stlfunctions.lua
@@ -135,17 +135,12 @@ local function Mode()
 	  }
 	  local mode_color = {
 		  n = colors.green,
-		  i = colors.blue,v=colors.magenta,[''] = colors.blue,V=colors.blue,
-		  c = colors.red,no = colors.magenta,s = colors.orange,S=colors.orange,
-		  [''] = colors.orange,ic = colors.yellow,R = colors.purple,Rv = colors.purple,
-		  cv = colors.red,ce=colors.red, r = colors.cyan,rm = colors.cyan, ['r?'] = colors.cyan,
-		  ['!']  = colors.green,t = colors.green,
-		  c  = colors.purple,
-		  ['r?'] = colors.red,
-		  ['r']  = colors.red,
-		  rm = colors.red,
+		  i = colors.blue,
+		  v = colors.magenta,[''] = colors.magenta,V = colors.magenta,
 		  R  = colors.yellow,
-		  Rv = colors.magenta,
+		  s = colors.orange,S=colors.orange, [''] = colors.orange,
+		  t = colors.red,
+		  c  = colors.purple,
 	  }
 	  local vim_mode = vim.fn.mode()
 	  vim.api.nvim_command('hi StatusLineMode guifg='..mode_color[vim_mode]..' gui=bold')
